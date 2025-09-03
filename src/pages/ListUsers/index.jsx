891af656753a2ec11bd5dc32052c5ced7c93cd66
Felipe Januario <felipejanuarioalves@outlook.com>
2025-09-03 02:17:32 -0300

    Implement code changes to enhance functionality and improve performance

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -1,32 +1,74 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import api from "../../services/api";
 
 import Button from "../../components/Button";
 import TopBackground from "../../components/TopBackground";
 
+import Trash from "../../assets/trash.svg";
 
+import {
+  CardUsers,
+  Container,
+  ContainerUsers,
+  Title,
+  TrashIcon,
+  AvatarUsers,
+} from "./styles";
 
 function ListUsers() {
+  const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function getUsers() {
       try {
-        const response = await api.get("/usuarios");
-        console.log(response.data); // aqui você pega os dados mesmo
+        const { data } = await api.get("/usuarios");
+
+        if (Array.isArray(data)) {
+          setUsers(data); // agora pega direto do backend
+        } else {
+          setUsers([]);
+        }
       } catch (error) {
         console.error("Erro ao buscar usuários:", error);
+        setUsers([]);
       }
     }
 
     getUsers();
   }, []);
-  
+
   return (
-    <div>
+    <Container>
       <TopBackground />
-      <h1>Listagem de Usuários</h1>
-      <Button>Voltar</Button>
-    </div>
+      <Title>Listagem de Usuários</Title>
+
+      <ContainerUsers>
+        {users.map((user) => (
+          <CardUsers key={user.id}>
+            <AvatarUsers
+              src={`https://avatar.iran.liara.run/public?username=${user.id}`} // usa ID do banco
+              alt={`Avatar de ${user.name}`}
+            />
+            <div>
+              <h3>{user.name}</h3>
+              <p>{user.email}</p>
+              <p>{user.age} anos</p>
+            </div>
+            <TrashIcon
+              src={Trash}
+              alt="icone-lixo"
+              onClick={() => console.log(`Deletar usuário ${user.id}`)}
+            />
+          </CardUsers>
+        ))}
+      </ContainerUsers>
+
+      <Button type="button" onClick={() => navigate("/")}>
+        Voltar
+      </Button>
+    </Container>
   );
 }
 
